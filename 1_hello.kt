ea12f1c8df85d7828196df78ccdc5752e4e6c72a
MJ <32310825+Michaeljivan@users.noreply.github.com>
2018-01-02 10:36:08 -0800

    Create 1_hello.kt

---
+++ 1_hello.kt
@@ -0,0 +1,15 @@
+//hello function in kotlin
+
+fun greet(){
+  println("Hello")
+}
+
+fun sum(a: Int, b: Int): Int {
+  return a + b
+}
+
+//variable assignment
+val a: Int = 10
+val b: Int = 20
+c = 3   //deferred assignment
+d = 4   
